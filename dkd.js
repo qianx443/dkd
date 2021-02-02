ca7da5b6e388f621b21ffa0a25f98ab8e6653935
qianx443 <74093378+qianx443@users.noreply.github.com>
2021-02-02 22:44:41 +0800

    Update dkd.js

--- dkd.js
+++ dkd.js
@@ -12,9 +12,9 @@
 获取方式，进入提现页面，选择需要自动提现的面额点击提现获取
 多看点自动任务
 */
+const axios = require("axios");
 const exec = require('child_process').execSync
 const fs = require('fs')
-const download = require('download')
 const $ = new Env('多看点');
 //let dkdurl = $.getdata('dkdurl')
 //let dkdhd = dkdhd
